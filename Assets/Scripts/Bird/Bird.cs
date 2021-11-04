4273486482bcf951b9d99b4f5e00059ddf11385b
shram78 <ra00275@gmail.com>
2021-11-04 14:48:10 +0300

    all comlete

--- Assets/Scripts/Bird/Bird.cs
+++ Assets/Scripts/Bird/Bird.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(BirdMover))]
 public class Bird : MonoBehaviour
@@ -8,25 +9,29 @@ public class Bird : MonoBehaviour
     private BirdMover _mover;
     private int _score;
 
+    public event UnityAction GameOver;
+    public event UnityAction<int> ScoreChanged;
+
     private void Start()
     {
         _mover = GetComponent<BirdMover>();
+    }
+    public  void IncreaseScore()
+    {
+        _score++;
+        ScoreChanged?.Invoke(_score);
     }
 
     public void ResetPlayer()
     {
         _score = 0;
+        ScoreChanged?.Invoke(_score);
         _mover.ResetBird();
     }
 
-    public  void IncreaseScore()
-    {
-        _score++;
-    }
 
     public void Die()
     {
-        Debug.Log("Я умер");
-        Time.timeScale = 0;
+        GameOver?.Invoke();
     }
 }
